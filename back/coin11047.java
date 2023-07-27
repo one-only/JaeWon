0c2b42cd73302a0d6945814cca4c3e34e91f98f1
YangJaeWon <yang7458@naver.com>
2023-07-27 20:54:48 +0900

    동전0

---
+++ back/coin11047.java
@@ -0,0 +1,31 @@
+package back;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class coin11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = n -1 ; i >= 0 ; i--) {
+            if(coin[i] <= k) {
+                count += k/coin[i];
+                k %= coin[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
