5456d053c3f88e15fab367699585e724c03ed4cc
YangJaeWon <yang7458@naver.com>
2023-09-27 23:55:54 +0900

    feat: 1. 안전영역 구하기(BOJ_2468)       2. 그림(BOJ_1926)       3. 단지번호 붙이기(BOJ_2667)

---
+++ baeck/BOJ_1926.java
@@ -0,0 +1,72 @@
+package baeck;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+//그림
+public class BOJ_1926 {
+    static int[][] map;
+    static boolean[][] visited;
+    static int n, m;
+    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int maxArea = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    int area = bfs(i, j);
+                    maxArea = Math.max(maxArea, area);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+        System.out.println(maxArea);
+    }
+    static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+        int area = 1;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    area++;
+                }
+            }
+        }
+
+        return area;
+    }
+}
